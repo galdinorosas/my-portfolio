bad5692a1ea29c3983126355c9f3cc899a153832
galdinorosas <dino.rosas@gmail.com>
2016-03-13 22:53:48 -0700

    final changes added to my personal website.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,22 @@ $(document).ready(function(){
 
 	$('.aboutMeLink').on('click', function(){
 		$('#homeContainer').css('display',  'none');
+		$('#projectsContainer').css('display', 'none');
 		$('#aboutMeContainer').css('display','block');
+	});
+
+	$('.dropdown-menu li:nth-child(1) a').on('click', function(){
+		$('#homeContainer').css('display',  'none');
+		$('#aboutMeContainer').css('display','none');
+		$('#projectsContainer').css('display', 'block');
+	});
+
+	$('.navList li:nth-child(3) a').on('click', function(){
+		$('#homeContainer').css('display',  'none');
+		$('#aboutMeContainer').css('display','none');
+		$('#projectsContainer').css('display', 'none');
+		$('#interestsContainer').css('display', 'block');
+
 	});
 
 
